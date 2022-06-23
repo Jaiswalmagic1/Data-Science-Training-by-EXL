d06cd39978b3fefc114552246d3c516d0b2ceca3
Jaiswalmagic1 <85571249+Jaiswalmagic1@users.noreply.github.com>
2022-06-23 07:27:17 +0530

    Add files via upload

---
+++ sort.py
@@ -0,0 +1,8 @@
+'''
+Create a list of length 7 (int, floats) and then sort the members in descending.
+'''
+
+list = [1, 5, 6 ,8 ,3 ,5 ,3.4, 6.8, 2.4, 0.1, 23.67]
+
+list.sort(reverse = True)
+print(list)
